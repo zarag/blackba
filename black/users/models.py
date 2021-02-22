c6aea03bb19e86c9b035d2431b0fa83a631b75a3
zarag <arza.grbic@gmail.com>
2021-02-22 20:45:11 +0100

    Make birthday nullable

--- black/users/models.py
+++ black/users/models.py
@@ -11,7 +11,7 @@ class User(AbstractUser):
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
-    birthday = models.DateField(_("birthday"), blank=True)
+    birthday = models.DateField(_("birthday"), null=True)
     last_name = None  # type: ignore
 
     def get_absolute_url(self):
