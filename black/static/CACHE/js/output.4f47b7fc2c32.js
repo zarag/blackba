d83627ec907063828afbae075fd6e8fdf00a0ddd
zarag <arza.grbic@gmail.com>
2021-03-13 16:33:58 +0100

    Coupon frontend - Initial

---
+++ black/static/CACHE/js/output.4f47b7fc2c32.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('li.active').removeClass('active');$('a[href="'+location.pathname+'"]').closest('li').addClass('active');$('.carousel').each(function(){$(this).find('.carousel-item').first().addClass('active')});});;
--- black/static/js/project.js
+++ black/static/js/project.js
@@ -2,4 +2,5 @@
 $(document).ready(function() {
     $('li.active').removeClass('active');
     $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
-  });
+    $('.carousel').each(function(){ $(this).find('.carousel-item').first().addClass('active')});
+});
