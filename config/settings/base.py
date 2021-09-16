d70c211d1375527491f93460bcc7dce1f0d87d00
zarag <arza.grbic@gmail.com>
2021-09-16 10:01:38 +0200

    authentication with new design

--- config/settings/base.py
+++ config/settings/base.py
@@ -70,6 +70,7 @@ THIRD_PARTY_APPS = [
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
+    # 'allauth.socialaccount.providers.facebook',
     # "django_celery_beat",
     "phonenumber_field",
     "versatileimagefield",
