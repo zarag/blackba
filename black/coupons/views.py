fecda6f0bb1ccc67fcc528d95d4da1ccd5a5628d
zarag <arza.grbic@gmail.com>
2021-10-17 20:45:53 +0200

    Pagination; initial

--- black/coupons/views.py
+++ black/coupons/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import JsonResponse
 from django.shortcuts import render
@@ -5,7 +6,6 @@ from django.template.loader import render_to_string
 from django.utils.timezone import now
 from django.views import generic
 from django.views.generic import DetailView
-from django.core.paginator import Paginator
 
 from .models import Coupon
 
@@ -40,18 +40,21 @@ def coupon_list_json(request):
     ctx = {}
     user_input = request.GET.get("inputValue")
     user_input = user_input if user_input else ""
-    page = request.GET.get('page', 1)
+    page = request.GET.get("page", 1)
     query_result = Coupon.objects.filter(
         Q(title__icontains=user_input), end_time__gte=now()
     ).order_by("-start_time")
-    paginator = Paginator(query_result, 10)
+    paginator = Paginator(query_result, 3)
     query_result = paginator.page(page)
     ctx["coupons"] = query_result
     ctx["top_offers"] = Coupon.objects.filter(Q(top_offer=True))
+    ctx["number_of_pages"] = paginator.num_pages
+    ctx["page_number"] = query_result.number
+    ctx["has_next"] = query_result.has_next()
+    ctx["has_previous"] = query_result.has_previous()
     if request.is_ajax():
         html = render_to_string(
-            template_name="coupons/coupons_search_results.html",
-            context={"coupons": query_result},
+            template_name="coupons/coupons_search_results.html", context=ctx
         )
         data_dict = {"html_from_view": html}
         return JsonResponse(data=data_dict, safe=False)
--- black/static/js/project.js
+++ black/static/js/project.js
@@ -1,37 +1,76 @@
 /* Project specific Javascript goes here. */
-$(document).ready(function() {
-    $('li.active').removeClass('active');
-    $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
-    $('.carousel').each(function(){ $(this).find('.carousel-item').first().addClass('active')});
-    // $('#id_birthday').datepicker({firstDay: 1,
-    //     dateFormat: "dd/mm/yyyy",
-    //     defaultDate: "16/06/2017",
-    //     language: 'bs',
-    //     today: "Danas",
-    //     dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
-    //     days: ["Sunnday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
-    // });
+$(document).ready(function () {
+  $('li.active').removeClass('active');
+  $('a[href="' + location.pathname + '"]').closest('li').addClass('active');
+  $('.carousel').each(function () { $(this).find('.carousel-item').first().addClass('active') });
+  // $('#id_birthday').datepicker({firstDay: 1,
+  //     dateFormat: "dd/mm/yyyy",
+  //     defaultDate: "16/06/2017",
+  //     language: 'bs',
+  //     today: "Danas",
+  //     dayNamesShort: ['So','Mo','Di','Mi','Do','Fr','Sa'],
+  //     days: ["Sunnday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"],
+  // });
 
-    let timeoutID = null;
+  let timeoutID = null;
 
-    $("#coupon-search-text").keyup(function (e) {
-        clearTimeout(timeoutID);
-        var input = $('#coupon-search-text').val();
+  $("#coupon-search-text").keyup(function (e) {
+    clearTimeout(timeoutID);
+    var input = $('#coupon-search-text').val();
 
-        timeoutID = setTimeout(() => {
-            $.ajax({
-                url: '/coupons/search',
-                data: {
-                  'inputValue': input
-                },
-                dataType: 'json',
-                success: function (data) {
-                  document.getElementById('search-results').innerHTML = data['html_from_view'];
-                },
-                error: function (e) {
-                    console.log(e);
-                }
-              });
-        }, 500);
+    timeoutID = setTimeout(() => {
+      $.ajax({
+        url: '/coupons/search',
+        data: {
+          'inputValue': input
+        },
+        dataType: 'json',
+        success: function (data) {
+          document.getElementById('search-results').innerHTML = data['html_from_view'];
+        },
+        error: function (e) {
+          console.log(e);
+        }
+      });
+    }, 500);
+  });
+
+  $("#search-results").on('click', '#couponsnext', function() {
+    var search_input = $('#coupon-search-text').val();
+    var page_number = $('#coupons-page-number').val();
+    $.ajax({
+      url: '/coupons/search',
+      data: {
+        'inputValue': search_input,
+        'page': Number(page_number) + 1
+      },
+      dataType: 'json',
+      success: function (data) {
+        document.getElementById('search-results').innerHTML = data['html_from_view'];
+      },
+      error: function (e) {
+        console.log(e);
+      }
     });
+  });
+
+
+  $("#search-results").on('click', '#couponsprevious', function() {
+    var search_input = $('#coupon-search-text').val();
+    var page_number = $('#coupons-page-number').val();
+    $.ajax({
+      url: '/coupons/search',
+      data: {
+        'inputValue': search_input,
+        'page': Number(page_number) - 1
+      },
+      dataType: 'json',
+      success: function (data) {
+        document.getElementById('search-results').innerHTML = data['html_from_view'];
+      },
+      error: function (e) {
+        console.log(e);
+      }
+    });
+  });
 });
